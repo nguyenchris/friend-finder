9811818441b48546b72e3a03ca9ddbece3cc819d
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-02-12 02:08:13 -0700

    added css styling to home.html

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.json());
 
 const htmlRoutes = require('./app/routes/htmlRoutes');
 const apiRoutes = require('./app/routes/apiRoutes');
 
+app.use(express.static(path.join(__dirname, 'app/public')));
 app.use(htmlRoutes);
 app.use('/api', apiRoutes);
 
